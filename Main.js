cb40a8c0c3b503e66522c6f2ad4b140f41e14abc
JoseCueto725222 <71628294+JoseCueto725222@users.noreply.github.com>
2023-01-08 07:26:58 +0400

    Proof of concept
    Assessment 2 - Cueto I

---
+++ Main.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tablink of tabcontents){
+        tabcontents.classList.remove("active-link");
+    }
+}
+
